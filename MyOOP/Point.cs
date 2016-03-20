82f50a89b29c22d1cdfb734a6fb196c23c37864b
Sergei Buzuikin <s.buzuikin@hotmail.com>
2016-03-20 23:09:21 +0300

    Add point

---
+++ MyOOP/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyOOP
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point() {
+        }
+
+        public Point(int x, int y, char sym) {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
